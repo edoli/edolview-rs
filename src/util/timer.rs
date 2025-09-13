a0580d53a854823ab70f5f4ebbe7f0a1edeb1e17
Daniel Jeon <edoli.mia@gmail.com>
2025-09-13 11:45:48 +0900

    Add timer

---
+++ src/util/timer.rs
@@ -0,0 +1,33 @@
+use std::time::Instant;
+
+pub struct ScopedTimer<'a> {
+    name: &'a str,
+    t0: Instant,
+}
+
+impl<'a> ScopedTimer<'a> {
+    pub fn new(name: &'a str) -> Self {
+        Self {
+            name,
+            t0: Instant::now(),
+        }
+    }
+
+    pub fn reset(&mut self) {
+        self.t0 = Instant::now();
+    }
+
+    pub fn elapsed(&self) -> std::time::Duration {
+        self.t0.elapsed()
+    }
+
+    pub fn print_elapsed(&self) {
+        eprintln!("[{}] took: {:.2?}", self.name, self.t0.elapsed());
+    }
+}
+
+impl<'a> Drop for ScopedTimer<'a> {
+    fn drop(&mut self) {
+        eprintln!("[{}] took: {:.2?}", self.name, self.t0.elapsed());
+    }
+}
