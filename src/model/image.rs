d565fd82c9bbe22b48a2bab44ff53d84462fe519
Daniel Jeon <edoli.mia@gmail.com>
2025-09-13 11:43:52 +0900

    Clean up

--- src/model/image.rs
+++ src/model/image.rs
@@ -5,7 +5,7 @@ pub struct ImageSpec {
     pub width: i32,
     pub height: i32,
     pub channels: i32,
-    pub cv_type: i32, // OpenCV type, e.g. CV_8U, CV_32F
+    pub dtype: i32, // OpenCV type, e.g. CV_8U, CV_32F
 }
 
 impl ImageSpec {
@@ -14,15 +14,15 @@ impl ImageSpec {
             width: mat.cols(),
             height: mat.rows(),
             channels: mat.channels(),
-            cv_type: mat.depth(),
+            dtype: mat.depth(),
         }
     }
 
     pub fn total_bytes(&self) -> usize {
-        (self.width as usize) * (self.height as usize) * (self.channels as usize) * self.cv_type.cv_type_bytes()
+        (self.width as usize) * (self.height as usize) * (self.channels as usize) * self.dtype.cv_type_bytes()
     }
 
     pub fn bytes_per_pixel(&self) -> usize {
-        (self.channels as usize) * self.cv_type.cv_type_bytes()
+        (self.channels as usize) * self.dtype.cv_type_bytes()
     }
 }
--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,2 +1,3 @@
 pub mod bool_ext;
 pub mod cv_ext;
+pub mod timer;
