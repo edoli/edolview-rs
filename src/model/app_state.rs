d466524edd84969ab63bd510e58e320bde3195d9
Daniel Jeon <edoli.mia@gmail.com>
2025-09-15 23:33:01 +0900

    Move image io from app_state to image

--- src/model/app_state.rs
+++ src/model/app_state.rs
@@ -1,9 +1,12 @@
-use std::{fs, path::PathBuf};
+use std::path::PathBuf;
 
-use color_eyre::eyre::{eyre, Result};
-use opencv::{core, imgcodecs, imgproc, prelude::*};
+use color_eyre::eyre::Result;
 
-use crate::{model::MatImage, ui::gl::ShaderParams, util::{self, cv_ext::CvIntExt, math_ext::Vec2i}};
+use crate::{
+    model::MatImage,
+    ui::gl::ShaderParams,
+    util::math_ext::Vec2i,
+};
 
 pub struct AppState {
     pub path: Option<PathBuf>,
@@ -23,52 +26,8 @@ impl AppState {
     }
 
     pub fn load_from_path(&mut self, path: PathBuf) -> Result<()> {
-        if !path.exists() {
-            return Err(eyre!("Image does not exist: {:?}", path));
-        }
-
-        let img = {
-            let _timer = util::timer::ScopedTimer::new("imdecode");
-            // Read image using imread fails on paths with non-ASCII characters.
-            // imgcodecs::imread(path.to_string_lossy().as_ref(), imgcodecs::IMREAD_UNCHANGED)?
-
-            let data = fs::read(&path).map_err(|e| eyre!("Failed to read file bytes: {e}"))?;
-            let data_mat = core::Mat::new_rows_cols_with_data(1, data.len() as i32, &data)?;
-            imgcodecs::imdecode(&data_mat, imgcodecs::IMREAD_UNCHANGED)?
-        };
-
-        if img.empty() {
-            return Err(eyre!("Failed to load image"));
-        }
+        self.display = Some(MatImage::load_from_path(&path)?);
         self.path = Some(path);
-        self.display = Some(self.process(&img)?);
         Ok(())
-    }
-
-    fn process(&self, mat: &core::Mat) -> Result<MatImage> {
-        let mut mat_f32 = core::Mat::default();
-        let mut tmp = core::Mat::default();
-
-        let channels = mat.channels();
-        let color_convert = match mat.channels() {
-            1 => -1,
-            3 => imgproc::COLOR_BGR2RGB,
-            4 => imgproc::COLOR_BGRA2RGBA,
-            _ => {
-                return Err(eyre!("Unsupported image channels: {}", channels));
-            }
-        };
-        
-        if color_convert != -1 {
-            imgproc::cvt_color(&mat, &mut tmp, color_convert, 0, core::AlgorithmHint::ALGO_HINT_DEFAULT)?;
-        } else {
-            tmp = mat.clone();
-        }
-
-        let dtype = tmp.depth();
-        
-        tmp.convert_to(&mut mat_f32, core::CV_32F, 1.0 / dtype.alpha(), 0.0)?;
-        
-        Ok(MatImage::new(mat_f32, dtype))
     }
 }
