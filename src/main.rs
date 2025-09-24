8ac8228a59582b9b9c116dba18e3af1f0b8458c6
Daniel Jeon <edoli.mia@gmail.com>
2025-09-25 00:37:04 +0900

    Set icon

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 use clap::Parser;
 use color_eyre::eyre::{eyre, Result};
 use eframe::egui::{self, ViewportBuilder};
+use opencv::{core, imgcodecs, imgproc, prelude::*};
 use std::path::PathBuf;
 
 use crate::ui::ViewerApp;
@@ -13,25 +14,14 @@ mod res;
 mod ui;
 mod util;
 
+const ICON_DATA: &[u8] = include_bytes!("../icons/icon.png");
+
 /// Simple image viewer using OpenCV for decoding + egui for GUI.
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about=None)]
 struct Args {
     /// Path to the image file to open (optional)
     image: Option<PathBuf>,
-}
-
-fn parse_max_size(spec: &str) -> Result<(i32, i32)> {
-    if spec == "0x0" {
-        return Ok((0, 0));
-    }
-    let parts: Vec<_> = spec.split('x').collect();
-    if parts.len() != 2 {
-        return Err(eyre!("Invalid size format. Use WIDTHxHEIGHT"));
-    }
-    let w: i32 = parts[0].parse()?;
-    let h: i32 = parts[1].parse()?;
-    Ok((w, h))
 }
 
 fn main() -> Result<()> {
@@ -43,12 +33,37 @@ fn main() -> Result<()> {
         std::env::remove_var("WAYLAND_DISPLAY");
     }
 
-    println!("{}", opencv::core::get_build_information()?);
+    println!("{}", core::get_build_information()?);
+
+    let icon_raw = core::Mat::new_rows_cols_with_data(1, ICON_DATA.len() as i32, &ICON_DATA)?;
+    let icon_mat_bgra = imgcodecs::imdecode(&icon_raw, imgcodecs::IMREAD_UNCHANGED)?;
+    let mut icon_mat_rgba = core::Mat::default();
+    imgproc::cvt_color(
+        &icon_mat_bgra,
+        &mut icon_mat_rgba,
+        imgproc::COLOR_BGRA2RGBA,
+        0,
+        core::AlgorithmHint::ALGO_HINT_DEFAULT,
+    )?;
+    let icon_bytes = icon_mat_rgba.data_bytes();
+
+    let icon = if let Ok(icon_bytes) = icon_bytes {
+        egui::IconData {
+            rgba: icon_bytes.to_vec(),
+            width: icon_mat_rgba.cols() as u32,
+            height: icon_mat_rgba.rows() as u32,
+        }
+    } else {
+        eprintln!("Failed to load icon image; continuing without icon");
+        egui::IconData::default()
+    };
+
     let native_options = eframe::NativeOptions {
         viewport: ViewportBuilder::default()
             .with_title("edolview-rs")
             .with_inner_size(egui::vec2(1280.0, 720.0))
-            .with_min_inner_size(egui::vec2(720.0, 480.0)),
+            .with_min_inner_size(egui::vec2(720.0, 480.0))
+            .with_icon(icon),
         ..Default::default()
     };
 
