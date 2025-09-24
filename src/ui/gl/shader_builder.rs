2b202afcce21fd0cebaffde6d7624bf6a858639c
Daniel Jeon <edoli.mia@gmail.com>
2025-09-24 12:10:41 +0900

    move image_shader into code

--- src/ui/gl/shader_builder.rs
+++ src/ui/gl/shader_builder.rs
@@ -17,13 +17,12 @@ const COLOR_PROCESS_MONO_COLORMAP: &str = r#"
     vec3 cm = colormap(v);
 "#;
 
+const IMAGE_SHADER_CODE: &str = include_str!("image_shader.frag");
 
 impl ShaderBuilder {
     pub fn new() -> Self {
-        let base_shader = std::fs::read_to_string("assets/image_shader.frag").expect("Failed to read shader file");
-
-        let base_rgb_shader = base_shader.replace("%color_process%", COLOR_PROCESS_RGB_COLORMAP);
-        let base_mono_shader = base_shader.replace("%color_process%", COLOR_PROCESS_MONO_COLORMAP);
+        let base_rgb_shader = IMAGE_SHADER_CODE.replace("%color_process%", COLOR_PROCESS_RGB_COLORMAP);
+        let base_mono_shader = IMAGE_SHADER_CODE.replace("%color_process%", COLOR_PROCESS_MONO_COLORMAP);
         Self {
             base_rgb_shader,
             base_mono_shader,
@@ -31,7 +30,7 @@ impl ShaderBuilder {
     }
 
     pub fn build(&self, colormap_name: &str, is_mono: bool) -> String {
-        let colormap_code = if is_mono {    
+        let colormap_code = if is_mono {
             std::fs::read_to_string(&format!("assets/colormap/mono/{}.glsl", colormap_name))
                 .unwrap_or_else(|_| String::new())
         } else {
@@ -39,7 +38,7 @@ impl ShaderBuilder {
                 .unwrap_or_else(|_| String::new())
         };
         // TODO: show error if colormap file not found
-        
+
         let shader = if is_mono {
             self.base_mono_shader.replace("%colormap_function%", &colormap_code)
         } else {
