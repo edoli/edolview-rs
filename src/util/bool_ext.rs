fca16ca986d96acbc5f8ed21fee577aee1e96e41
Daniel Jeon <edoli.mia@gmail.com>
2025-09-24 09:59:35 +0900

    remove switch ext. rather use macro version

--- src/util/bool_ext.rs
+++ src/util/bool_ext.rs
@@ -1,17 +1,3 @@
-pub trait BoolExt {
-    fn switch<T>(self, val_true: T, val_false: T) -> T;
-}
-
-impl BoolExt for bool {
-    fn switch<T>(self, val_true: T, val_false: T) -> T {
-        if self {
-            val_true
-        } else {
-            val_false
-        }
-    }
-}
-
 #[macro_export]
 macro_rules! switch {
     ($cond:expr => $then:expr, $else:expr $(,)?) => {{
