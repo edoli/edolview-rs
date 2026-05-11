a5bf945e32a4c18958c321168523af2c6aa442ba
Daniel Jeon <edoli.mia@gmail.com>
2026-05-11 10:34:09 +0900

    Cleanup keyboard shortcuts using type alias

--- src/res/keyboard_shortcuts.rs
+++ src/res/keyboard_shortcuts.rs
@@ -1,29 +1,30 @@
 use eframe::egui::{Key, KeyboardShortcut, ModifierNames, Modifiers};
 
+type KSC = KeyboardShortcut;
+
 pub const IS_MAC: bool = cfg!(target_os = "macos");
 
-pub const SELECT_ALL_SC: KeyboardShortcut = KeyboardShortcut::new(Modifiers::COMMAND, Key::A);
-pub const SELECT_NONE_SC: KeyboardShortcut = KeyboardShortcut::new(Modifiers::NONE, Key::Escape);
-pub const COPY_SC: KeyboardShortcut = KeyboardShortcut::new(Modifiers::COMMAND, Key::C);
-pub const OPEN_FROM_CLIPBOARD_SC: KeyboardShortcut =
-    KeyboardShortcut::new(Modifiers::CTRL.plus(Modifiers::SHIFT), Key::V);
-pub const SAVE_IMAGE_SC: KeyboardShortcut = KeyboardShortcut::new(Modifiers::COMMAND, Key::S);
-pub const BOOKMARK_PANEL_TOGGLE: KeyboardShortcut = KeyboardShortcut::new(Modifiers::NONE, Key::B);
-pub const BOOKMARK_ADD: KeyboardShortcut = KeyboardShortcut::new(Modifiers::COMMAND, Key::B);
-pub const BOOKMARK_PREV: KeyboardShortcut = KeyboardShortcut::new(Modifiers::NONE, Key::OpenBracket);
-pub const BOOKMARK_NEXT: KeyboardShortcut = KeyboardShortcut::new(Modifiers::NONE, Key::CloseBracket);
+pub const SELECT_ALL_SC: KSC = KSC::new(Modifiers::COMMAND, Key::A);
+pub const SELECT_NONE_SC: KSC = KSC::new(Modifiers::NONE, Key::Escape);
+pub const COPY_SC: KSC = KSC::new(Modifiers::COMMAND, Key::C);
+pub const OPEN_FROM_CLIPBOARD_SC: KSC = KSC::new(Modifiers::CTRL.plus(Modifiers::SHIFT), Key::V);
+pub const SAVE_IMAGE_SC: KSC = KSC::new(Modifiers::COMMAND, Key::S);
+pub const BOOKMARK_PANEL_TOGGLE: KSC = KSC::new(Modifiers::NONE, Key::B);
+pub const BOOKMARK_ADD: KSC = KSC::new(Modifiers::COMMAND, Key::B);
+pub const BOOKMARK_PREV: KSC = KSC::new(Modifiers::NONE, Key::OpenBracket);
+pub const BOOKMARK_NEXT: KSC = KSC::new(Modifiers::NONE, Key::CloseBracket);
 
-pub const RESET_VIEW: KeyboardShortcut = KeyboardShortcut::new(Modifiers::NONE, Key::R);
+pub const RESET_VIEW: KSC = KSC::new(Modifiers::NONE, Key::R);
 
-pub const FULLSCREEN_TOGGLE: KeyboardShortcut = KeyboardShortcut::new(Modifiers::NONE, Key::F11);
+pub const FULLSCREEN_TOGGLE: KSC = KSC::new(Modifiers::NONE, Key::F11);
 
-pub const ZOOM_IN: KeyboardShortcut = KeyboardShortcut::new(Modifiers::NONE, Key::Equals);
-pub const ZOOM_OUT: KeyboardShortcut = KeyboardShortcut::new(Modifiers::NONE, Key::Minus);
+pub const ZOOM_IN: KSC = KSC::new(Modifiers::NONE, Key::Equals);
+pub const ZOOM_OUT: KSC = KSC::new(Modifiers::NONE, Key::Minus);
 
-pub const NAVIGATE_PREV: KeyboardShortcut = KeyboardShortcut::new(Modifiers::NONE, Key::ArrowLeft);
-pub const NAVIGATE_NEXT: KeyboardShortcut = KeyboardShortcut::new(Modifiers::NONE, Key::ArrowRight);
-pub const NAVIGATE_ASSET_PREV: KeyboardShortcut = KeyboardShortcut::new(Modifiers::NONE, Key::PageUp);
-pub const NAVIGATE_ASSET_NEXT: KeyboardShortcut = KeyboardShortcut::new(Modifiers::NONE, Key::PageDown);
+pub const NAVIGATE_PREV: KSC = KSC::new(Modifiers::NONE, Key::ArrowLeft);
+pub const NAVIGATE_NEXT: KSC = KSC::new(Modifiers::NONE, Key::ArrowRight);
+pub const NAVIGATE_ASSET_PREV: KSC = KSC::new(Modifiers::NONE, Key::PageUp);
+pub const NAVIGATE_ASSET_NEXT: KSC = KSC::new(Modifiers::NONE, Key::PageDown);
 
 pub const PRESET_KEYS: [Key; 9] = [
     Key::Num1,
@@ -37,18 +38,18 @@ pub const PRESET_KEYS: [Key; 9] = [
     Key::Num9,
 ];
 
-const fn preset_shortcuts(modifiers: Modifiers) -> [KeyboardShortcut; PRESET_KEYS.len()] {
-    let mut shortcuts = [KeyboardShortcut::new(Modifiers::NONE, Key::Num0); PRESET_KEYS.len()];
+const fn preset_shortcuts(modifiers: Modifiers) -> [KSC; PRESET_KEYS.len()] {
+    let mut shortcuts = [KSC::new(Modifiers::NONE, Key::Num0); PRESET_KEYS.len()];
     let mut index = 0;
     while index < PRESET_KEYS.len() {
-        shortcuts[index] = KeyboardShortcut::new(modifiers, PRESET_KEYS[index]);
+        shortcuts[index] = KSC::new(modifiers, PRESET_KEYS[index]);
         index += 1;
     }
     shortcuts
 }
 
-pub const PRESET_SAVE_SHORTCUTS: [KeyboardShortcut; PRESET_KEYS.len()] = preset_shortcuts(Modifiers::COMMAND);
-pub const PRESET_APPLY_SHORTCUTS: [KeyboardShortcut; PRESET_KEYS.len()] = preset_shortcuts(Modifiers::ALT);
+pub const PRESET_SAVE_SHORTCUTS: [KSC; PRESET_KEYS.len()] = preset_shortcuts(Modifiers::COMMAND);
+pub const PRESET_APPLY_SHORTCUTS: [KSC; PRESET_KEYS.len()] = preset_shortcuts(Modifiers::ALT);
 
 pub const MODIFIER_NAMES: ModifierNames = ModifierNames {
     is_short: false,
